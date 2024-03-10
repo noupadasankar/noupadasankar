7e0ed3487b13d23c65f24e8f551f6f9d75ff4cf5
noupadasankar <157972865+noupadasankar@users.noreply.github.com>
2024-03-10 22:43:36 +0530

    Amazon.js

--- Amazon.js
+++ Amazon.js
@@ -1,4 +1,4 @@
-import { todayDeal } from "./todayDeal.js"
+import { todayDeal } from "./Amazon_todaydeal.js"
 
 let slideBtnLeft = document.getElementById("slide-btn-left")
 let slideBtnRight = document.getElementById("slide-btn-right")
